9d74abfdb2182396cff929a640bdcab513bd40eb
mr-shalom <hellomr.shalom@gmail.com>
2025-11-10 17:24:14 +0100

    Feature Mobile Navigation

--- src/components/dashboards/studentdashboard/MainDisplay.jsx
+++ src/components/dashboards/studentdashboard/MainDisplay.jsx
@@ -1,15 +1,20 @@
 import { CiBellOn } from "react-icons/ci";
+import { FiMenu } from "react-icons/fi";
 
-function MainDisplay({ children }) {
+function MainDisplay({ children, setIsOpen }) {
   return (
     <section className="px-4 py-2 flex flex-col flex-grow max-h-full">
-      <header className="flex flex-grow items-center gap-2 justify-between">
-        <h1 className="md:text-xl text-sm font-semibold drop-shadow-lg">
-          Dashboard
+      <header className="flex flex-grow items-center gap-2 justify-between overflow-hidden">
+        <h1 className="md:text-xl text-sm font-semibold drop-shadow-lg flex items-center gap-6">
+          <FiMenu
+            onClick={() => setIsOpen(true)}
+            className="block lg:hidden cursor-pointer text-lg"
+          />
+          <span>Dashboard</span>
         </h1>
         <CiBellOn className="text-lg cursor-pointer" />
       </header>
-      <main className="flex-grow h-full">{children}</main>
+      <main className="flex-grow h-full overflow-hidden">{children}</main>
     </section>
   );
 }
--- src/components/dashboards/studentdashboard/StudentDashboard.jsx
+++ src/components/dashboards/studentdashboard/StudentDashboard.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import SideBar from "./SideBar";
 import MainDisplay from "./MainDisplay";
 import { Outlet } from "react-router-dom";
+import MobileNav from "./MobileNav";
 function StudentDashboard() {
   const [activeTab, setActiveTab] = useState("dashboard");
+  const [isOpen, setIsOpen] = useState(false);
+
   function handleActiveTab(tab) {
     setActiveTab(tab);
   }
   return (
-    <section className="min-h-dvh flex ">
+    <section className="min-h-dvh flex relative overflow-hidden">
       <SideBar onhandleActiveTab={handleActiveTab} activeTab={activeTab} />
-      <MainDisplay activeTab={activeTab}>
+      {isOpen ? <MobileNav setIsOpen={setIsOpen} /> : null}
+      <MainDisplay activeTab={activeTab} setIsOpen={setIsOpen}>
         <Outlet />
       </MainDisplay>
     </section>
