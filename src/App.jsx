807dd7211808768bb758921d17a1edcddb21d451
mr-shalom <hellomr.shalom@gmail.com>
2025-11-23 23:45:03 +0100

    FIX route in APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,39 +25,6 @@ import Analytics from "./components/dashboards/admindashboard/Analytics";
 function App() {
   const router = createBrowserRouter([
     {
-      element: <MainLayout />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          element: <StudentDashboard />,
-          children: [
-            {
-              index: true,
-              element: <Navigate to="dashboard" replace />,
-            },
-            { path: "dashboard", element: <Dashboard /> },
-            { path: "student's profile", element: <StudentProfile /> },
-            { path: "exams", element: <ExamComponent /> },
-            { path: "history", element: <ExamsHistory /> },
-          ],
-        },
-        {
-          path: "admindashboard",
-          element: <AdminDashboard />,
-          children: [
-            {
-              index: true,
-              element: <Navigate to="profile" replace />,
-            },
-            // { path: "profile", element: <Profile /> },
-            // { path: "registerstudent", element: <RegisterStudent /> },
-            // { path: "uploadexams", element: <UploadExams /> },
-            // { path: "uploadanswers", element: <UploadAnswers /> },
-          ],
-        },
-      ],
-    },
-    {
       path: "/",
       element: <AuthLayout />,
       children: [
